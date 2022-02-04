3a5def6d84209fd6cc96864344ee13d1068587cb
Keila Beatriz <keila_bvs@outlook.com>
2022-02-03 23:11:02 -0300

    🚧 Aula 2 - NextJs - Criando componentes e paginas da aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,28 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <div className={"styles.title"}>Hello World</div>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publication <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
